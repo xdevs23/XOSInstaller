d0615be3ac1bd2b8f2c9e1efd06ac9eb7f23294e
xdevs23 <xdevs23@outlook.com>
2016-06-18 14:55:14 +0200

    InstallerGUI: use LanguageManager

--- InstallerGUI/InstallerGUI/Main.vb
+++ InstallerGUI/InstallerGUI/Main.vb
@@ -1,3 +1,12 @@
-﻿Public Class Main
+﻿Imports xdevs23.Localization
+
+Public Class Main
+
+    Private LangManager As LanguageManager
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LangManager = New LanguageManager()
+        LangManager.AutoApplyLanguage(Me)
+    End Sub
 
 End Class
