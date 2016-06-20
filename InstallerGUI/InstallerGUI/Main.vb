4a42958a55be523f5c76408aba617e3e13182fbb
xdevs23 <xdevs23@outlook.com>
2016-06-20 18:04:17 +0200

    InstallerGUI: load some fonts, set RobotoCondensed

--- InstallerGUI/InstallerGUI/Main.vb
+++ InstallerGUI/InstallerGUI/Main.vb
@@ -5,23 +5,34 @@ Imports xdevs23.Localization
 Public Class Main
 
     Private LangManager As LanguageManager
-    Private Roboto, RobotoCondensed As New PrivateFontCollection()
+    Private RobotoCondensed, RobotoLight, RobotoThin As PrivateFontCollection
 
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Load fonts
+        RobotoCondensed = New PrivateFontCollection
+        RobotoLight = New PrivateFontCollection
+        RobotoThin = New PrivateFontCollection
         For Each FontFile As String 
                 In Directory.GetFiles("fonts/", "Roboto-*.ttf", SearchOption.AllDirectories)
-            Roboto.AddFontFile(FontFile)
+            Debug.WriteLine("Debug: Loading font file " & FontFile & " for main window")
+            If FontFile.Contains("Thin") Then
+                RobotoThin.AddFontFile(FontFile)
+            ElseIf FontFile.Contains("Light") Then
+                RobotoLight.AddFontFile(FontFile)
+            End If
         Next
         For Each FontFile As String
                 In Directory.GetFiles("fonts/", "RobotoCondensed-*.ttf", SearchOption.AllDirectories)
             RobotoCondensed.AddFontFile(FontFile)
         Next
-        Font = New Font(Roboto.Families(0), 9.25, FontStyle.Regular)
+        Font = New Font(RobotoCondensed.Families(0), 9.25, FontStyle.Regular)
 
         ' Load languages
         LangManager = New LanguageManager()
         LangManager.AutoApplyLanguage(Me)
+
+        ' Align & style installer title
+
     End Sub
 
 End Class
