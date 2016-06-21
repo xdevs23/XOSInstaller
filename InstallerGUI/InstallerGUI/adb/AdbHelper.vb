1a96acf1b618db364bdcf27346a71159446a455b
xdevs23 <xdevs23@outlook.com>
2016-06-21 17:47:03 +0200

    XOSInstaller: add the adb prebuilts

---
+++ InstallerGUI/InstallerGUI/adb/AdbHelper.vb
@@ -0,0 +1,33 @@
+﻿Public Class AdbHelper
+
+    Private Shared AdbExec As String = _
+        IIf(My.Computer.Info.OSFullName.ToLower() _
+            .Contains("unix"), "prebuilts/linux/adb/adb", "prebuilts\adb\adb.exe").ToString()
+
+    Public Shared Function ExecuteAdbCommand(command As String) As String
+        Dim p As New ProcessStartInfo
+
+        p.FileName  = AdbExec
+        p.Arguments = command
+
+        ' Don't show a window!
+        p.WindowStyle = ProcessWindowStyle.Hidden
+        p.CreateNoWindow = True
+
+        ' Prepare to catch up the output
+        p.RedirectStandardOutput = True
+        p.UseShellExecute = False
+
+        ' Start the process
+        Dim proc As New Process()
+        proc.StartInfo = p
+        proc.Start()
+
+        Dim result As String = proc.StandardOutput.ReadToEnd()
+        Console.WriteLine(result)
+        Debug.WriteLine(result)
+        proc.WaitForExit()
+        Return result
+    End Function
+
+End Class
---
+++ InstallerGUI/Prebuilts/Main.vb
@@ -0,0 +1,8 @@
+﻿Module Main
+
+    ' This file is only to ensure that the prebuilts are copied
+    Sub Main(argv As String())
+
+    End Sub
+
+End Module
