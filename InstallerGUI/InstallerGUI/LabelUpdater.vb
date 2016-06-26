f71aabc4748d42b1c0b7101ae6261aeae4a38eda
xdevs23 <xdevs23@outlook.com>
2016-06-26 18:50:29 +0200

    InstallerGUI: add LabelUpdater

---
+++ InstallerGUI/InstallerGUI/LabelUpdater.vb
@@ -0,0 +1,27 @@
+﻿Imports xdevs23.Localization
+
+Public Class LabelUpdater
+
+    Private Shared LM As LanguageManager
+
+    Public Shared Sub SetLangManager(ByRef LangManager As LanguageManager)
+        LM = LangManager
+    End Sub
+
+    Public Shared Sub UpdateLabel(Lbl As Label, Text As String)
+        Lbl.Text = Text
+    End Sub
+
+    Public Shared Sub UpdateLabel(Lbl As Label, Format As String, Text As String)
+        UpdateLabel(Lbl, String.Format(Format, Text))
+    End Sub
+
+    Public Shared Sub UpdateLabel(Lbl As Label, Format As String, ParamArray Texts As String())
+        UpdateLabel(Lbl, String.Format(Format, Texts))
+    End Sub
+
+    Public Shared Sub UpdateLabel(Lbl As Label, ParamArray Texts As String())
+        UpdateLabel(Lbl, String.Format(LM.GetString(Lbl.Tag.ToString()), Texts))
+    End Sub
+
+End Class
